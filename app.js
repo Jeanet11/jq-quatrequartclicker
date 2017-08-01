8d8cb2a4e7630a20c769108279862997d50566d6
Jeanne <jeanne.tiramani@hotmail.fr>
2017-08-01 19:18:30 +0200

    compteur et alertes ok

--- app.js
+++ app.js
@@ -1,5 +1,20 @@
 $(document).ready(function(){
-	//votre code
+	var count = 0;
 
+	$(".cake-is-a-lie").click(function() {
+		count++;
+		$(".counter").html(count);
+
+	var array = [ "tata", "toto", "tutu","titi"];
+	var item = parseInt(Math.random() * array.length);
+	
+	
+	if (count%20 === 0) {
+
+		alert(array[item]);
+	}	
+
+	});
+	
 
 });
